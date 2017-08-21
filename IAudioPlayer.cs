a6a5c71ffe6b884300befc924aff9ad426d80a40
Yana Wilkins <yav9774@gmail.com>
2017-08-21 12:11:17 -0400

    initial commit

---
+++ IAudioPlayer.cs
@@ -0,0 +1,10 @@
+namespace Interfaces
+{
+    public interface IAudioPlayer
+    {
+        //music
+        //record voices
+        //shuffle songs
+         
+    }
+}
---
+++ Smartphone.cs
@@ -0,0 +1,10 @@
+namespace Interfaces
+{
+    public class Smartphone : IAudioPlayer; ITxtMessage
+    {
+        public string Display {get; set;}
+        public string Ding {get; set;}
+
+        public bool Functional {get; set;}
+    }
+}
